48f4e03282f47f54650b2ad842870c8e1d39a589
amber-king <amberking@pursuit.org>
2023-07-28 14:05:20 -0400

    App.js setup - working progress

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import logo from "./logo.svg";
 import "./App.css";
-// import React from "react";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./Components/Home";
+import Quotes from "./Components/QuotePage";
+import NavBar from "./Components/NavBar";
 
 function App() {
   return (
     <div className="App">
-      <div>
-        <h1>Love Thyself ❤️</h1>
-        <p>
-          This app strives to brighten your day with a daily quote to enchance
-          your mood for the day, Enjoy! 😁
-        </p>
-      </div>
-      ;
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/topic/:topic" element={<Quotes />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
