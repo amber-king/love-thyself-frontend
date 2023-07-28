eac4432631dcf23b19af50e09ca679d6c22b9eef
amber-king <amberking@pursuit.org>
2023-07-28 18:42:11 -0400

    new frontend setup,no code yet

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
 import NavBar from "./Components/NavBar";
+import Confidence from "./Components/Confidence";
+import Happiness from "./Components/Happiness";
+import Inspiration from "./Components/Inspiration";
+import Today from "./Components/Today";
+import Truth from "./Components/Truth";
+
 
 function App() {
   return (
@@ -11,6 +17,11 @@ function App() {
         <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/api/quotes/confidence" element={<Confidence />} />
+          <Route path="/api/quotes/happiness" element={<Happiness />} />
+          <Route path="/api/quotes/inspiration" element={<Inspiration />} />
+          <Route path="/api/quotes/today" element={<Today />} />
+          <Route path="/api/quotes/truth" element={<Truth />} />
         </Routes>
       </Router>
     </div>
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -7,7 +7,7 @@ export default function Home() {
       <br></br>
       <center>
         <img
-          src="https://media.tenor.com/8RLOQeOq_I0AAAAM/be-kind-to-yourself-kindness.gif"
+          src="https://media.tenor.com/UxLjfnJs1owAAAAd/be-kind-to-yourself-hugs.gif"
           alt="self-hug-img"
         ></img>
       </center>
