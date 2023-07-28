dd9686e582c87e5364984aff3143e608e29f5db3
amber-king <amberking@pursuit.org>
2023-07-28 10:11:35 -0400

    beggining home page work

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+// import React from "react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div>
+        <h1>Love Thyself ❤️</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This app strives to brighten your day with a daily quote to enchance
+          your mood for the day, Enjoy! 😁
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      ;
     </div>
   );
 }
